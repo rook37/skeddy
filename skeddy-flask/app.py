04b0a19c1bb7ca1c20182ce21faca3fc2e71203d
rook37 <33361387+rook37@users.noreply.github.com>
2025-03-19 15:17:46 -0500

    Initializing Flask server

---
+++ skeddy-flask/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import skeddy
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    try:
+        if request.method == 'POST':
+            team_name = request.form['team']
+            league_id = request.form['id']
+            season = request.form['season']
+        if(team_name and league_id and season):
+            output = skeddy.fetch_schedule(team_name,league_id,season)
+        else:
+            return jsonify({"success": False, "error": "Missing required parameters"}), 400
+        
+        if not output or not output["success"]:
+                return jsonify({"success": False, "error": output["error"]}), 500
+
+        csv_path = output["csv_path"]
+        return send_file(csv_path, as_attachment=True)
+
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5555, debug=True)
+
